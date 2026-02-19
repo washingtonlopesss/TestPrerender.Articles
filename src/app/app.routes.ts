dbaca6a5f7f6eb01f079a72dcf282d9854a7ff02
Washington Lopes <washingtonlopes2003@gmail.com>
2026-02-18 21:59:13 -0300

    Ajustando de rota para o github

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,10 @@ import { ArtigoPage } from './features/artigos/artigo.page';
 import { HomePage } from './pages/home/home.page';
 
 export const routes: Routes = [
+  { path: 'TestPrerender.Articles', redirectTo: '', pathMatch: 'full' },
+
   { path: '', component: HomePage, title: 'Home' },
   { path: 'artigos/:slug', component: ArtigoPage, title: 'Artigo' },
+
   { path: '**', redirectTo: '' },
-];
+]
